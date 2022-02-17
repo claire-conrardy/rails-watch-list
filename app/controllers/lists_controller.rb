4b732d6ed388ffa1d332877b3c643fd8eaae2e6e
Claire Conrardy <claire.conrardy@gmail.com>
2022-02-17 14:47:04 -0500

    Routes and controller created for Lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,21 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+  end
+
+  private
+
+  def list_params
+  end
+end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
 
-
   validates :comment, length: { minimum: 6 }
   validates :movie_id, uniqueness: { scope: :list }
 end
