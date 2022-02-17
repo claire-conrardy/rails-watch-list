8e180f519f4001c1e6c8dff093c8e553c7d917e0
Claire Conrardy <claire.conrardy@gmail.com>
2022-02-17 15:20:53 -0500

    Lists views and controller done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,10 +12,17 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render 'lists/new'
+    end
   end
 
   private
 
   def list_params
+    params.require(:list).permit(:name)
   end
 end
